8ceef593d890cdd8bff631de305fcab98b20d36f
Sujith Sudarshan <sujith.sudarshan@example.com>
2022-01-31 15:37:22 +0530

    Make pmake run async

--- src/pmake.py
+++ src/pmake.py
@@ -1,5 +1,8 @@
 from pathlib import Path
-import subprocess
+import shutil
+import shlex
+import asyncio
+import pprint
 
 import click
 
@@ -10,6 +13,8 @@ def get_base_path_from_venv() -> Path:
         if part.lower() in ['venv', 'env', '.venv', '.env', 'venv.bak', 'env.bak']:
             break
         parts.append(part)
+    else:
+        return Path(__file__).parent.parent.resolve()
 
     if not parts:
         raise ValueError("Could not get the base folder path")
@@ -17,7 +22,14 @@ def get_base_path_from_venv() -> Path:
     return Path("\\".join(parts)).resolve()
 
 
-def clean(path: Path, *args, **kwargs) -> None:
+async def run(cmd: str):
+    path = get_base_path_from_venv()
+
+    proc = await asyncio.create_subprocess_shell(cmd, cwd=path)
+    _ = await proc.communicate()
+
+
+def clean(*args, **kwargs) -> None:
     """Delete all gen/build files
 
     - public/
@@ -26,51 +38,52 @@ def clean(path: Path, *args, **kwargs) -> None:
     pass
 
 
-def npm_install(path: Path, *args, **kwargs) -> None:
-    cmd = ['npm', 'install', '-D',
-           'tailwindcss@latest',
-           'postcss@latest',
-           'autoprefixer@latest',
-           '@tailwindcss/typography@latest']
+def npm_install(*args, **kwargs) -> None:
+    """Setup npm modules"""
+    cmd = shlex.split('npm install -D tailwindcss@latest postcss@latest autoprefixer@latest @tailwindcss/typography@latest')
+    cmd[0] = f'"{shutil.which(cmd[0])}"'
+    cmd = ' '.join(cmd)
 
-    subprocess.run(cmd, cwd=path, shell=True)
+    return cmd
 
 
-def tailwind(path: Path, *args, **kwargs) -> None:
+def tailwind(*args, **kwargs) -> None:
     """Launch watcher process"""
+    cmd = shlex.split('npx tailwindcss --input ./src/tailwind.css --output ./static/css/tailwind.css --watch')
+    cmd[0] = f'"{shutil.which(cmd[0])}"'
+    cmd = ' '.join(cmd)
 
-    cmd = ['npx', 'tailwindcss',
-           '--input', './src/tailwind.css',
-           '--output', './static/css/tailwind.css',
-           '--watch']
-
-    subprocess.run(cmd, cwd=path, shell=True)
+    return cmd
 
 
-def zola(path: Path, drafts: bool = False, *args, **kwargs) -> None:
+def zola(drafts: bool=False, *args, **kwargs) -> None:
     """Serve zola process"""
-    cmd = ['zola', 'serve',
-           '--interface', '127.0.0.1',
-           '--base-url', '127.0.0.1',
-           '--open']
+    cmd = shlex.split('zola serve --interface 127.0.0.1 --base-url 127.0.0.1 --open')
+    cmd[0] = f'"{shutil.which(cmd[0])}"'
 
     if drafts:
         cmd.append('--drafts')
 
-    subprocess.run(cmd, cwd=path, shell=True)
+    cmd = ' '.join(cmd)
+
+    return cmd
+
+
+async def main(cmds: list[list[str]]):
+    await asyncio.gather(*[run(cmd) for cmd in cmds])
 
 
 @click.command()
 @click.argument('cmd_args', nargs=-1)
 def make(cmd_args):
-    path = get_base_path_from_venv()
-
     if not cmd_args:
-        print("make [tailwind|zola[+drafts]|clean]")
+        print("make [clean] [tailwind] [zola[+drafts]]")
+
+    cmds = []
 
     for cmd in cmd_args:
         args = []
-        kwargs = {"path": path}
+        kwargs = {}
         if '+' in cmd:
             cmd, *args = cmd.split('+')
             kwargs |= {x: True for x in args}
@@ -79,4 +92,8 @@ def make(cmd_args):
 
         if func is not None and callable(func):
             print(f"Running {cmd} with {kwargs!s}")
-            func(**kwargs)
+            cmd = func(**kwargs)
+            if cmd is not None:
+                cmds.append(cmd)
+
+    asyncio.run(main(cmds))
